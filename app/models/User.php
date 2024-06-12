bfbebf97a4d2687199e5fc6d6e1fd05672f23fcc
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-06-12 23:27:07 +0000

    Created a log table in the database and logged all login attempts (good and bad). Logged username, attempt (good, bad), and time

--- app/models/User.php
+++ app/models/User.php
@@ -36,6 +36,13 @@ class User {
       // Check if any matches occur and return either true or false.
       return $statement->fetch() ? true : false;
     }
+    public function attemptLog($username, $attempt) {
+      $db = db_connect();
+      $statement = $db->prepare("insert into log (username, attempt) VALUES (:username, :attempt)");
+      $statement->bindValue(':username', $username);
+      $statement->bindValue(':attempt', $attempt);
+      $statement->execute();
+    }
     public function authenticate($username, $password) {
         /*
          * if username and password good then
@@ -53,12 +60,14 @@ class User {
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
 		
 		if (password_verify($password, $rows['password_hash'])) {
-			$_SESSION['auth'] = 1;
+			$this->attemptLog($username, 'good');
+      $_SESSION['auth'] = 1;
 			$_SESSION['username'] = ucwords($username);
 			unset($_SESSION['failedAuth']);
 			header('Location: /home');
 			die;
 		} else {
+      $this->attemptLog($username, 'bad');
 			if(isset($_SESSION['failedAuth'])) {
 				$_SESSION['failedAuth'] ++; //increment
 			}
